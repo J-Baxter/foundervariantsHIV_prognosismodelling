9a0dc92aba4cdd7a6e222f7afdb2ce8db76b8ec8
James <james.baxter@ed.ac.uk>
2021-06-01 19:34:15 +0100

    init prelim_models

---
+++ scripts/prelim_models&vis.R
@@ -0,0 +1,15 @@
+###################################################################################################
+###################################################################################################
+# Preliminary models and visualisation of published data concerning the relationship between founder
+# variant multiplicity, the decline in CD4+ T-Cells and set point viral load
+
+###################################################################################################
+###################################################################################################
+# Dependencies
+library(tidyr)
+library(lme4)
+library(dplyr)
+library(metafor)
+library(ggplot2)
+library(reshape2)
+library(ggplot2)
