44a239951f1776caea53fe316288ce0aef1786ec
James <james.baxter@ed.ac.uk>
2021-09-01 13:53:32 +0100

    donor data generation

--- scripts/prelim_models&vis.R
+++ scripts/prelim_models&vis.R
@@ -13,3 +13,37 @@ library(metafor)
 library(ggplot2)
 library(reshape2)
 library(ggplot2)
+
+
+# Donor data generation
+# Normal distibution of log spvl from Hollingsworth Cohort (Link). Mean = 4.35, SD = 0.47 
+DONORS <- 100000
+
+donor_spvl <- rnorm(DONORS, mean = 4.39, sd = 0.84) 
+
+ggplot() + geom_density(aes(x=donor_spvl), color = 'blue') + theme_bw()
+
+#Inclusion of other covariates
+#Sex
+sex <- c('m', "f")
+donor_sex <- sample(sex, DONORS, replace = T)
+
+#Age
+age <- c(NA, '15-24', '25-29', '30-39', '40-64')
+donor_age <- sample(age, DONORS, replace = T)
+
+#GUD
+gud <- c(NA, 'present', "absent")
+donor_gud <- sample(gud, DONORS, replace = T)
+
+#Subtype
+subtype <- c(NA, 'recombinant', 'a', 'c', 'd')
+donor_subtype <- sample(subtype, DONORS, replace = T)
+
+donor_data <- cbind.data.frame(donor_spvl, donor_sex, donor_age, donor_gud, donor_subtype)
+
+# Null model (Hollingsworth)
+# Predict recipient SPVL from donor SPVL using GLM fitted in Hollingsworth et al. to generate
+# Null distribution of recipient SPLV
+
+
