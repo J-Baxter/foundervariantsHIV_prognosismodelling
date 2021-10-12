b6200b19e859bf9a94c41bf3a93d34c6aa0df4cf
James <james.baxter@ed.ac.uk>
2021-10-12 18:17:57 +0100

    formulated generation of recipient data from a given correlation coefficient; improved notation and references

--- scripts/prelim_models&vis.R
+++ scripts/prelim_models&vis.R
@@ -1,7 +1,15 @@
 ###################################################################################################
 ###################################################################################################
-# Preliminary models and visualisation of published data concerning the relationship between founder
-# variant multiplicity, the decline in CD4+ T-Cells and set point viral load
+# Modelling the association between the number of variants and viral load
+# Script utilises methodology from Thompson et al 2019, https://github.com/robin-thompson/MultiplicityOfInfection
+# implemented in R by Katie Atkins https://github.com/katiito/HIV_eclipse_models
+
+# N Scenarios are presented:
+#1. Null hypothesis
+#2.
+#3.
+#4.
+
 
 ###################################################################################################
 ###################################################################################################
@@ -13,35 +21,44 @@ library(metafor)
 library(ggplot2)
 library(reshape2)
 library(ggplot2)
+source("populationdata_models.R")
+
+GetRecipVL <- function(donor_loads, h2){
+  n = length(donor_loads)
+  ssr = sum((donor_loads-mean(donor_loads))^2) # sum of squared residuals
+  e <- rnorm(n)
+  e <- resid(lm(e ~ donor_loads))
+  e <- e*sqrt((1-h2)/h2*ssr/(sum(e^2)))
+  recip_loads <- donor_loads + e
+  
+  return(recip_loads)
+}
+
+###################################################################################################
+# Data generation
+# Normal distibution of log spvl from Hollingsworth Cohort (Link). mean = 4.39, sd = 0.84
+# https://journals.plos.org/plospathogens/article?id=10.1371/journal.ppat.1000876
+donor_spvl <- rnorm(NPAIRS, mean = 4.39, sd = 0.84)
+
+# Heritability estimate
+R2 <- 0.35
+NPAIRS <- 1000
+
+# Generate recipient data
+recip_spvl <- GetRecipVL(donor_spvl, 0.35)
+
+# Check calcualted R2  == input (to run in test script)
+#summary(lm(recip_spvl ~ donor_spvl))$r.squared
+
+# Data frame of paired viral loads
+spvl_df <- cbind.data.frame(donor_spvl, recip_spvl)
 
 
 ###################################################################################################
-# Donor data generation
-# Normal distibution of log spvl from Hollingsworth Cohort (Link). Mean = 4.35, SD = 0.47 
-NPAIRS <- 25000
-
-donor_spvl <- rnorm(NPAIRS, mean = 4.39, sd = 0.84)
-recip_spvl <-sapply(donor_spvl, GetRecipVL)
-  #sapply(donor_spvl, rnorm, n = 1, sd = 1.15)
-summary(lm(recip_spvl ~ donor_spvl))
-
-GetRecipVL <- function(vl){
-  poss_recip <- rnorm(10000, mean = vl, sd = 1.15)
-  
-  random <- sample(1:10000, 1)
-  
-  while (poss_recip[random] < 0){
-    random <- sample(1:10000, 1)}
-  
-  out <- poss_recip[random]
-  
-  return(out)
-}
+# Probability that recipient infection is initiated by multiple founder variants
+# applies function written by Katie Atkins
 
 
-vl_df <- cbind.data.frame(donor_spvl, recip_spvl)
-
-diff <- donor_spvl - recip_spvl
 ggplot() +
   geom_density(aes(x=diff), color = 'blue')
 
@@ -57,7 +74,6 @@ ggplot() +
 
 diff <-  rnorm(NPAIRS, mean = 0.84, sd = 1.28)
 
-<<<<<<< HEAD
 
 paired_spvl <- cbind.data.frame(donor = donor_spvl, recipient= donor_spvl+diff)
 
@@ -86,122 +102,11 @@ test_3 <- ggplot() +
 
 test_panel <- cowplot::plot_grid(test_1, test_3, test_2, ncol = 3, labels = 'AUTO')
 test_panel
-=======
+
+
 fit <- lm(recip_vl_mid ~ donor_spvl, vl_df) %>% summary()
 
 ggplot()+
   geom_point(aes(x = donor_spvl,y = recip_spvl), data = vl_df) 
   
-  
->>>>>>> a36418d709f274f4848337e7d1d693d85197b453
-###################################################################################################
-# Recipient covariates required for model
-# Sex
-# 1 = male, 0 = female
-recipient_sex <- rbinom(NPAIRS, 1, 0.5) %>% cbind.data.frame() #altered to represent bias in transmission direction?
-colnames(recipient_sex) <- 'sex'
-
-# Age
-age <- c(NA, '15-24', '25-29', '30-39', '40-64') %>% as.factor()
-recipient_age <- sample(age, NPAIRS, replace = T) %>% data.table() %>% one_hot(sparsifyNAs = TRUE)
-colnames(recipient_age) <- c('a1', 'a2', 'a3', 'a4')
-
-# GUD
-gud <- c(NA, 'absent', 'present') %>% as.factor()
-recipient_gud <- sample(gud, NPAIRS, replace = T) %>% data.table() %>% one_hot(sparsifyNAs = TRUE)
-colnames(recipient_gud) <- c('gud_n', 'gud_p')
-
-# Subtype
-subtype <- c(NA, 'a', 'c', 'd','recombinant') %>% as.factor()
-recipient_subtype <- sample(subtype, NPAIRS, replace = T) %>% data.table() %>% one_hot(sparsifyNAs = TRUE)
-colnames(recipient_subtype) <- c('sub_a', 'sub_c', 'sub_d', 'sub_r')
-
-# Position in chain
-chain <- c('index', 'second') %>% as.factor()
-recipient_chain <- sample(chain, NPAIRS, replace = T) %>% data.table() %>% one_hot(sparsifyNAs = TRUE)
-colnames(recipient_chain) <- c('chain_index', 'chain_second')
-
-recipient_covar <- cbind.data.frame(recipient_sex, recipient_age, recipient_gud, recipient_subtype, recipient_chain)
-
-
-###################################################################################################
-# Null model (Hollingsworth)
-# Predict recipient SPVL from donor SPVL using GLM fitted in Hollingsworth et al. to generate
-# Null distribution of recipient SPLV
-#recip_spvl = i - 0.15*sex - 0.15*a1 - 0.32*a2 + 0.35*a3 + 0.73*a4 + 0.42*gud_n + 0.62*gud_p + 0.068*sub_a + 1.85*sub_c + 0.39*sub_D + 0.78*sub_R + 0.42*chain_index + 0.00*chain_second
-
-NullModel <- function(donor, recipient){
-  
-  recip <- donor - 0.15*recipient$sex - 0.15*recipient$a1 - 0.32*recipient$a2 + 0.35*recipient$a3 + 0.73*recipient$a4 +
-    0.42*recipient$gud_n + 0.62*recipient$gud_p + 0.068*recipient$sub_a + 1.85*recipient$sub_c + 0.39*recipient$sub_d +
-    0.78*recipient$sub_r + 0.42*recipient$chain_index + 0.00*recipient$chain_second
-  
-  return(recip)
-}
-
-
-recipient_spvl <- numeric()
-
-for (i in 1:length(donor_spvl)){
-  recipient_spvl[i] <- NullModel(donor_spvl[i], recipient_covar [i,])
-}
-
-
-###################################################################################################
-# Null Panel
-# A) Distribution of donor SPVL
-donor_plot <- ggplot() +
-  geom_density(aes(x=donor_spvl), color = 'blue') + 
-  geom_density(aes(x=recip_vl), color = 'red') + 
-  theme_bw()+
-  scale_y_continuous(limits = c(0,0.5), expand = c(0,0))+
-  scale_x_continuous(limits = c(0,12), expand = c(0,0))+
-  xlab('Log10  SPVL')
-
-# B) Distribution of recipient SPVL
-recip_plot <- ggplot() + 
-  geom_density(aes(x=recip_vl), color = 'red') + 
-  theme_bw()+
-  scale_y_continuous(limits = c(0,0.5), expand = c(0,0))+
-  scale_x_continuous(limits = c(0,12), expand = c(0,0))+
-  xlab('Log10 Recipient SPVL')
-
-# C) Donor-recipient paired difference in SPVL A
-<<<<<<< HEAD
-paired_spvl <- cbind.data.frame(donor = donor_spvl, recipient= recipient_spvl)
-
-diff_plot <- ggscatter(paired_spvl, x='donor', y = 'recipient', add = "reg.line", shape = 4) +
-=======
-diff_plot <- ggplot() +
-  geom_point(aes(x=donor_spvl, y = recip_vl), shape = 4)+ 
->>>>>>> a36418d709f274f4848337e7d1d693d85197b453
-  theme_bw()+
-  xlab('Log10 Donor SPVL') +
-  ylab('Log10 Recipient SPVL') +
-  theme(legend.position = "none") + 
-  scale_y_continuous(limits = c(0,12), expand = c(0,0))+
-  scale_x_continuous(limits = c(0,12), expand = c(0,0))+
-  stat_cor(aes(label = paste(..rr.label..)), label.x = 8, label.y = 11, digits = 3)
-  
-library(ggpubr)
-
-
-null_panel <- cowplot::plot_grid(donor_plot, recip_plot, diff_plot, ncol = 3, labels = 'AUTO')
-
-null_panel
-
-
-<<<<<<< HEAD
-=======
-# Calculate difference between donor-recipient pairs
-
-
-# for a given 
-
-
-
-
->>>>>>> a36418d709f274f4848337e7d1d693d85197b453
-
-###################################################################################################
-###################################################################################################
+ 
