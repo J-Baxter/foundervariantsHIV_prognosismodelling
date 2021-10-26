4e3874a04cc90db5f658f24cf790978f7a95dca1
James <james.baxter@ed.ac.uk>
2021-10-26 13:42:45 +0100

    init stage of donor infection

---
+++ scripts/stage_of_donor_infection.R
@@ -0,0 +1,22 @@
+###################################################################################################
+###################################################################################################
+# Modelling the association between the number of variants and viral load
+# Script utilises methodology from Thompson et al 2019, https://github.com/robin-thompson/MultiplicityOfInfection
+# implemented in R by Katie Atkins https://github.com/katiito/HIV_eclipse_models
+# Testing hypothesis 1: Stage of infection in donor
+
+###################################################################################################
+###################################################################################################
+# Dependencies
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(ggpmisc)
+library(parallel)
+library(cowplot)
+
+source("./scripts/populationdata_models.R")
+source("./scripts/misc_functions.R")
+source("./scripts/null_models.R")
+load("./nulldata.RData")
