73c41c3aa149a7e686a49d1c0392a44300e2f627
James Baxter <james.baxter@ed.ac.uk>
2023-01-18 15:13:12 +0000

    init main

---
+++ scripts/main.R
@@ -0,0 +1,17 @@
+# Main Analysis for "RECONCILING THE FOUNDER VARIANT MULTIPLICITY OF HIV-1 INFECTION WITH THE RATE
+# OF CD4+ DECLINE"
+# J Baxter
+
+# This script combines the functions for the analysis of paired SPVL distributions, occuring in
+# three main stages:
+
+# 1. Initialisation of study population
+# 2. Null hypothesis
+# 3. Ecological hypothes
+
+
+# Dependencies
+source('./scripts/dependencies.R')
+source('./scripts/init_population.R')
+source('./scripts/populationdata_models.R')
+source('./scripts/populationdata_acrossVL_models.R')
