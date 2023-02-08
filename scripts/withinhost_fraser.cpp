37a69d4cf187f43604b0042a07104a7b067f22e0
James Baxter <james.baxter@ed.ac.uk>
2023-02-08 19:09:55 +0000

    model transcribed, not run

--- scripts/withinhost_fraser.cpp
+++ scripts/withinhost_fraser.cpp
@@ -10,7 +10,7 @@ using namespace Rcpp;
 // Erlang shape fixed at 4 for infectious categories and 2 for recovery categories
 
 // [[Rcpp::export]]
-List SIR_cpp_model(NumericVector times,NumericVector state,
+List Fraser_cpp_model(NumericVector times,NumericVector state,
                    List parms){
   
   // Declare State Variables
@@ -24,12 +24,14 @@ List SIR_cpp_model(NumericVector times,NumericVector state,
   double z = state["z"]; //anti-HIV CTLs
   double k_cd4 = state["k_cd4"];
   double k_cd8 = state["k_cd8"];
+  double pk_cd4 = state["k_cd4"];
+  double pk_cd8 = state["k_cd8"];
  
   
 
   // Declare Parms
-  double cd4_gamma = parms["cd4_gamma"]; //daily thymic production of new cd4
-  double cd8_gamma = parms["cd8_gamma"]; //daily thymic production of new cd8
+  double lambda_cd4 = parms["lambda_cd4"]; //daily thymic production of new cd4
+  double lambda_cd8 = parms["lambda_cd8"]; //daily thymic production of new cd8
   double a_0 = parms["a_0"]; //average rate of T cell activation per antigenic exposure
   double mu = parms["mu"]; //daily rate of non-antigen-driven homeostaatic T cell division
   double x_s = parms["x_s"]; //relative t cell pool size, below which t cell activation fails due to exhaustion
@@ -46,26 +48,26 @@ List SIR_cpp_model(NumericVector times,NumericVector state,
   double bc_ratio = parms["b"]; // ratio of viral production rate in productively infected cells and viral lifetime
   double sigma = parms["sigma"]; // max rat of CTL killing of HIV infected cells
   //double N_PB = parms["N_PB"];
-  double cd4_theta = parms["cd4_theta"]; // average clearance rate in antigenic exposure model
-  double cd8_theta = parms["cd8_theta"]; // average exposure rate in antigenic exposure model
+  double theta_cd4 = parms["theta_cd4"]; // average clearance rate in antigenic exposure model
+  double theta_cd8 = parms["theta_cd8"]; // average exposure rate in antigenic exposure model
   double e_cd4 = parms["e_cd4"];
   double e_cd8 = parms["e_cd8"];
 
   
   //Antigenic Exposure and T Cell Activation
-  double a_4 = a_0*k_cd4*(cd4/(cd4+x_s));
-  double a_8 = a_0*k_cd8*(cd8/(cd8+x_s));
+  double a_cd4 = a_0*k_cd4*(cd4/(cd4+x_s));
+  double a_cd8 = a_0*k_cd8*(cd8/(cd8+x_s));
   
-  double pk_CD4 = cd4_theta*(k_cd4+1)*pk_cd4[] - (e_cd4 + cd4_theta * k_cd4)*pk_cd4 + e_cd4*p_kcd4; 
-  double pk_CD8 = ;
+  double pk_CD4 = theta_cd4*(k_cd4+1)*(pk_cd4+1) - (e_cd4 + theta_cd4 * k_cd4)*pk_cd4 + e_cd4*(pk_cd4-1); //Ask Katie
+  double pk_CD8 = theta_cd8*(k_cd8+1)*(pk_cd8+1) - (e_cd8 + theta_cd8 * k_cd8)*pk_cd8 + e_cd8*(pk_cd8-1);
   double epsilon = infectious_active/(infectious_active+ infectious_0);
-  double homeostasis = cd4_gamma + cd8_gamma + mu + 2*a_0*(2*p_a + 1);
+  double homeostasis = lambda_cd4 + lambda_cd8 + mu + 2*a_0*(2*p_a + 1);
 
   // T Cell Dynamics
-  double dcd4 = cd4_gamma-a_4 + 2*p_a*mu_a*cd4_activated + mu*cd4 - homeostasis*cd4*(cd4 + cd8);
-  double dcd8 = cd8_gamma-a_8 + 2*p_a*mu_a*cd8_activated + mu*cd8 - homeostasis*cd8*(cd4 + cd8);
-  double dcd4_activated = a_4 - mu_a*cd4_activated - beta*v*cd4_activated;
-  double dcd8_activated = a_8 - mu_a*cd8_activated;
+  double dcd4 = lambda_cd4-a_cd4 + 2*p_a*mu_a*cd4_activated + mu*cd4 - homeostasis*cd4*(cd4 + cd8);
+  double dcd8 = lambda_cd8-a_cd8 + 2*p_a*mu_a*cd8_activated + mu*cd8 - homeostasis*cd8*(cd4 + cd8);
+  double dcd4_activated = a_cd4 - mu_a*cd4_activated - beta*v*cd4_activated;
+  double dcd8_activated = a_cd8 - mu_a*cd8_activated;
   
   //HIV Viral Dynamics and CTL Control of HIV
   double dinfectious_active = (1-frac_latent)*beta*v*cd4_activated + alpha_latent*infectious_latent - (alpha + sigma*epsilon*z)*infectious_active;
@@ -73,7 +75,7 @@ List SIR_cpp_model(NumericVector times,NumericVector state,
   double dz = d*(z_0 - z) + p*epsilon*z;
   double dv = infectious_active*bc_ratio;
   
-  NumericVector res_vec = NumericVector::create(dcd4, dcd8, dcd4_activated, dcd8_activated, dinfectious_active, dinfectious_latent, dz, dv);
+  NumericVector res_vec = NumericVector::create(dcd4, dcd8, dcd4_activated, dcd8_activated, dinfectious_active, dinfectious_latent, dz, dv, k_cd4, k_cd8, pk_cd4, pk_cd8);
   
   List res = List::create(res_vec);
   
