d5596a0d0fe74873c37bcbd25ff5de3836d35ea2
James Baxter <james.baxter@ed.ac.uk>
2023-02-09 12:46:29 +0000

    additional package dependencies

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -5,7 +5,7 @@
 # increase in viral load.
 ###################################################################################################
 ############################# Check necessary packages are available and install; import if not.
-pkgs <- c('tidyverse', 'parallel', 'ggpmisc', 'ggpubr', 'ggprism', 'cowplot', 'scales', 'sysfonts') 
+pkgs <- c('tidyverse', 'parallel', 'ggpmisc', 'ggpubr', 'ggprism', 'cowplot', 'scales', 'sysfonts', 'showtext', 'showtextdb', 'ggExtra') 
 
 for (pkg in pkgs){
   
