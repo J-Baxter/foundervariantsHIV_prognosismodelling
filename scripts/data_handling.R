e492ec8f3ccc83076bd6f02c24d14e6d009a919a
J-Baxter <james.baxter@ed.ac.uk>
2023-02-28 19:07:47 +0000

    post-processing finalised

--- scripts/data_handling.R
+++ scripts/data_handling.R
@@ -1,4 +1,13 @@
-t <- transmitterspvl_variantdist %>% 
+t <- cbind.data.frame(transmitter = pop$transmitter, prob_recip_multiple) %>% 
   rename(probTransmissionPerSexAct_average = probTransmissionPerSexAct) %>%
-  rename_with(function(x) gsub('\\.(?<=\\V)', '\\1_average.\\2',x, perl= TRUE), .cols = !contains('chronic') & contains('V')) %>%
-  pivot_longer(cols = c(contains('chronic'),contains('average')), names_to = c('type', 'stage', 'variants'), names_pattern = "()-()-()", values_to = 'p') #1) everything before last _ 2)after last _ and before last . 3) after last .
+  rename_with(function(x) gsub('\\.(?<=\\V)', '\\1_average.\\2',x, perl= TRUE), 
+              .cols = !contains('chronic') & contains('V')) %>%
+  pivot_longer(cols = c(contains('chronic'),contains('average')), 
+               names_to = c('type', 'stage', 'variants'), 
+               names_pattern = "(^[^_]+)(_[^.]*)(.*)", 
+               values_to = 'p') %>% 
+  #regex wrong above, but tidied up below
+  mutate(stage = gsub('^.*_', '', stage)) %>%
+  mutate(variants = str_remove_all(variants,'[:alpha:]|[:punct:]') %>% as.numeric()) 
+
+str(t)
