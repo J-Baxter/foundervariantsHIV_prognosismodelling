8611ca331e3573d19101f9f18dc491c83021dbe7
James Baxter <james.baxter@ed.ac.uk>
2023-03-01 19:01:22 +0000

    added nlme to dependencies

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -5,7 +5,7 @@
 # increase in viral load.
 ###################################################################################################
 ############################# Check necessary packages are available and install; import if not.
-pkgs <- c('tidyverse', 'parallel', 'ggpmisc', 'ggpubr', 'ggprism', 'cowplot', 'scales', 'sysfonts', 'showtext', 'showtextdb', 'ggExtra', 'ggridges') 
+pkgs <- c('tidyverse', 'parallel', 'ggpmisc', 'ggpubr', 'ggprism', 'cowplot', 'scales', 'sysfonts', 'showtext', 'showtextdb', 'ggExtra', 'nlme') 
 
 for (pkg in pkgs){
   
