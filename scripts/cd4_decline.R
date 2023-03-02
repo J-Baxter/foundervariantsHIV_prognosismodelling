f6b520666f88c31092a89f95bd04686ce3840937
James Baxter <james.baxter@ed.ac.uk>
2023-03-02 17:46:30 +0000

    minor refactoring

--- scripts/cd4_decline.R
+++ scripts/cd4_decline.R
@@ -1,14 +1,22 @@
 # CD4 decline model
 # from Disentangling Human Tolerance and Resistance Against HIV
 # Regoes et al. PLoS Biology 2014
+# CD4 decline is 'change in CD4+ T cells per microlitre per day'
+
+
 
 CD4Decline <- function(log10SPVL,age,sex){
+  
+  ##################################################
+  # parameters from Regoes et al. PLoS Biology 2014
+  ##################################################
+  
   alpha_f = -0.005866
   eta_m = 0.000491
   c_f = -0.000167
   z_m = 0.000001
   
-  # Create sex dummy variable
+  # Participant sex as dummy variable
   sex <- ifelse(sex =='M', 1, 0)
   
   deltaCD4 <- (alpha_f + eta_m*sex + (c_f + z_m*sex)*age)*(log10SPVL)**2
