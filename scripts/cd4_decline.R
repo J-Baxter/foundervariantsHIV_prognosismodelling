624df2741eaa8987e67f9bca06e6833035d3e096
James Baxter <james.baxter@ed.ac.uk>
2023-03-02 11:56:32 +0000

    function for CD4 decline (including age and sex)

--- scripts/cd4_decline.R
+++ scripts/cd4_decline.R
@@ -1,5 +1,18 @@
 # CD4 decline model
+# from Disentangling Human Tolerance and Resistance Against HIV
+# Regoes et al. PLoS Biology 2014
 
-function(log10SPVL){
-  deltaCD4 <- 0.0111*log10(log10SPVL))**2
-}
+
+CD4Decline <- function(log10SPVL,age,sex){
+  alpha_f = -0.005866
+  eta_m = 0.000491
+  c_f = -0.000167
+  z_m = 0.000001
+  
+  # Create sex dummy variable
+  sex <- ifelse(sex =='M', 1, 0)
+  
+  deltaCD4 <- (alpha_f + eta_m*sex + (c_f + z_m*sex)*age)*(log10SPVL)**2
+  
+  return(deltaCD4)
+} 
