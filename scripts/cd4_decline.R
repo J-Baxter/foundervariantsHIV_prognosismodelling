563b9b807a3c99ed558e8933f62f2bf78157102b
J-Baxter <james.baxter@ed.ac.uk>
2023-03-06 10:39:46 +0000

    single effect model

--- scripts/cd4_decline.R
+++ scripts/cd4_decline.R
@@ -3,7 +3,12 @@
 # Regoes et al. PLoS Biology 2014
 # CD4 decline is 'change in CD4+ T cells per microlitre per day'
 
+cd4_data <- read_delim('./data/pbio.1001951.s006.tsv', delim = '\t' )
 
+# -0.1 + 0.051*log10(V) - 0.017*(log10(V))**2
+cd4_model <- lm(CD4.decline ~ poly(spVL, degree =1), data = cd4_data )
+summary(cd4_model)
+confint(cd4_model, level = 0.95)
 
 CD4Decline <- function(log10SPVL,age,sex){
   
