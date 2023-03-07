483668ae0befa5840b78f209c10ddd1828278d6b
J-Baxter <james.baxter@ed.ac.uk>
2023-03-07 20:25:56 +0000

    data import

--- scripts/cd4_decline.R
+++ scripts/cd4_decline.R
@@ -4,6 +4,7 @@
 # CD4 decline is 'change in CD4+ T cells per microlitre per day'
 
 
+cd4_data <- read_delim('./data/pbio.1001951.s006.tsv', delim = '\t' )
 
 # -0.1 + 0.051*log10(V) - 0.017*(log10(V))**2
 cd4_model <- lm(CD4.decline ~ spVL + I(spVL**2), data = cd4_data )
