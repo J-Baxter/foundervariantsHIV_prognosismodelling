c1a329ab7ebdc1641fb78c33c2cf099e7dde32e2
James Baxter <james.baxter@ed.ac.uk>
2023-03-09 17:04:37 +0000

    nls figures temporarily incorporated

--- scripts/figures.R
+++ scripts/figures.R
@@ -177,3 +177,66 @@ ggplot(pops_com, aes(x = transmitter_log10SPVL, recipient_log10SPVL, colour = hy
     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0))
   )+
   annotation_logticks() 
+
+fig_3a= ggplot(nls_df) + 
+  geom_density(aes(x = recipient_log10SPVL, fill = sim), alpha = 0.4)+
+  my_theme +
+  scale_fill_brewer(palette = 'OrRd')+
+  scale_y_continuous(expand = c(0,0))+
+  scale_x_continuous(name = expression(paste("Recipient SPVL", ' (', Log[10], " copies ", ml**-1, ')')),
+                     limits = c(0,8), expand = c(0,0))+ theme(
+                       legend.position = 'none')
+fig3_data <- combined %>% 
+  filter(variants == 1) %>%
+  filter (stage == 'average')
+
+fig_3b <- ggplot(fig3_data , 
+                 aes(x = recipient,
+                     y = 1 - p,
+                     colour = model))+
+  geom_point(shape = 3, size = 4) +
+  scale_colour_brewer(palette = 'OrRd')+
+  scale_x_log10(name = expression(paste("Recipient SPVL", ' (', Log[10], " copies ", ml**-1, ')')),
+                limits = c(1, 10**8),
+                expand = c(0,0),
+                breaks = trans_breaks("log10", function(x) 10**x),
+                labels = trans_format("log10", math_format(.x))) +
+  scale_y_continuous(name = 'P(Multiple Founder Recipient)',
+                     expand = c(0,0),
+                     limits = c(0,0.6),
+                     breaks = seq(0, 0.6, by = 0.1)) +
+  annotation_logticks(sides = 'b') +
+  my_theme+ theme(
+    legend.position = 'none')
+
+
+
+fig_3c <- ggplot(fig3_data , 
+                 aes(y = CD4_decline,
+                     x = 1-p,
+                     colour = model,
+                     group = model))+
+  geom_point(shape = 3, size = 4) +
+  scale_colour_brewer(palette = 'OrRd')+
+  scale_y_continuous(name = expression(paste(Delta, ' CD4+ ', mu, l**-1, ' ', day**-1)),  #
+                     expand = c(0,0),
+                     limits = c(-2,2)) +
+  scale_x_continuous(name = 'P(Multiple Founder Recipient)',
+                     expand = c(0,0),
+                     limits = c(0,0.6),
+                     breaks = seq(0, 0.6, by = 0.1))+
+  geom_smooth(method = 'lm', formula = 'y~x') +
+  
+  my_theme + theme(axis.title.y = element_text(family = 'sans'), 
+                   legend.position = 'none')
+
+
+
+grid_3 <- cowplot::plot_grid(fig_3a, fig_3b, fig_3c, align = 'hv', nrow = 1, labels = 'AUTO')
+
+legend <- get_legend(
+  fig_3a +theme(legend.position = "bottom")
+)
+grid_4 <- cowplot::plot_grid(grid_3,legend , nrow = 2, rel_heights = c(1, .1) )
+
+ggsave(paste0(figs_dir, '/test.eps'), device = cairo_ps , plot = grid_4, width = 16, height = 10)
