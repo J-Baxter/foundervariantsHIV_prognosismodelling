d2c65a7116172015f1593018186c7a4892b4e2b9
James Baxter <james.baxter@ed.ac.uk>
2023-03-09 15:54:50 +0000

    Import WeightPDF and InitSimTransmitter from init_populations.R

--- scripts/SimPop.R
+++ scripts/SimPop.R
@@ -1,3 +1,20 @@
+WeightPDF <- function(viralload){
+  alpha = -3.55
+  sigma <- 0.78/(sqrt(1 - ((2*alpha**2)/(pi*(1 + alpha**2)))))
+  mu <-  4.74 - (2*sigma*alpha)/(sqrt(2*pi*(1 + alpha**2)))
+  weight <-  (2/sigma)*dnorm((log10(viralload) - mu)/sigma)*pnorm(alpha*(log10(viralload) - mu)/sigma)
+  return(weight)
+} 
+
+
+InitSimTransmitter <- function(pop_size, transmitter_min, transmitter_max, sample_prob){
+  sim_range <- seq(transmitter_min, transmitter_max, length.out = pop_size)
+  sim_logspvl <- sample(sim_range, size = pop_size, prob = sample_prob, replace = T)
+  
+  return(sim_logspvl)
+}
+
+
 SimPop <- function(initpop, model, n){
   
   transmitter_prob <- sapply(pop$transmitter_log10SPVL, function(x) WeightPDF(x)/sum(WeightPDF(pop$transmitter_log10SPVL)))
