684e77bb7670a5a0a763c13e2d29ee9db7a02c35
James Baxter <james.baxter@ed.ac.uk>
2023-03-22 20:36:52 +0000

    function output in years

--- scripts/CD4_decline.R
+++ scripts/CD4_decline.R
@@ -1,15 +1,14 @@
-SimCD4Decline <- function(delta_cd4, init_cd4, theshold = 200){
+SimCD4Decline <- function(delta_cd4, init_cd4, threshold = 200){
   z <- init_cd4
   i <-  1
   
   while(z[i]>threshold){
-    
-    z[i+1] = z[i] - delta_cd4 
+    z[i+1] = z[i] - abs(delta_cd4)*365.25
     i = i + 1 
     
   }
   
-  return(z)
+  return(i-1)
   
 }
 
