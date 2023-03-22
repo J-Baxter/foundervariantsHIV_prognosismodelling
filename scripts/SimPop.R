fb0a91b3cf5aa77f79b9d953d9b56314526c5c7e
James Baxter <james.baxter@ed.ac.uk>
2023-03-22 18:52:08 +0000

    deletion of incorrect code; implementation of two lightweight funcs (pdf and sim donor)

--- scripts/SimPop.R
+++ scripts/SimPop.R
@@ -7,35 +7,19 @@ WeightPDF <- function(viralload){
 } 
 
 
-InitSimTransmitter <- function(pop_size, transmitter_min, transmitter_max, sample_prob){
-  sim_range <- seq(transmitter_min, transmitter_max, length.out = pop_size)
-  sim_logspvl <- sample(sim_range, size = pop_size, prob = sample_prob, replace = T)
+SimDonor <- function(n, transmitter_min, transmitter_max){
+  
+  spvl_vec <- seq(transmitter_min, transmitter_max, length.out = 10000)
+  
+  sim_probs <- sapply(spvl_vec, function(x) WeightPDF(x)/sum(WeightPDF(spvl_vec)))
+  
+  sim_logspvl <- sample(spvl_vec, size = n, prob = sim_probs, replace = F) %>%
+    cbind.data.frame('transmitter_log10SPVL' = .)
   
   return(sim_logspvl)
 }
 
 
-SimPop <- function(initpop, model, n, modeltype = 'linear'){
-  
-  stopifnot('transmitter_log10SPVL' %in% colnames(initpop))
-  t <- initpop['transmitter_log10SPVL']
-  
-  transmitter_prob <- sapply(t, function(x) WeightPDF(x)/sum(WeightPDF(t)))
-  
-  hist(transmitter_prob) #visual check - should look normal(ish) as already log
-  
-  sim_transmitter_log10SPVL <- InitSimTransmitter(pop_size = NPAIRS,
-                                                  transmitter_min = 1, 
-                                                  transmitter_max = 7, 
-                                                  sample_prob = transmitter_prob)
-  
-  hist(sim_transmitter_log10SPVL) #visual check - should look uniform
-  
-  sim_spvl <- predict(model, newdata= data.frame(transmitter_log10SPVL = sim_transmitter_log10SPVL)) %>% 
-    cbind.data.frame(sim_recipient_log10SPVL = ., sim_transmitter_log10SPVL = sim_transmitter_log10SPVL) %>%
-    mutate(across(.cols = everything(), .fns = ~ 10**.x, .names = "{str_remove(col, '_log10SPVL')}")) %>%
-    `colnames<-` (str_remove(colnames(.), 'sim_')) %>% mutate(model = modeltype)
-  
-  return(sim_spvl)
-}
+
+
 
