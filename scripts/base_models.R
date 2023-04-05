6f15e15bb5428db5c179ae22397d74457ecedff0
James Baxter <james.baxter@ed.ac.uk>
2023-04-05 11:30:39 +0100

    minor refactoring (SpVL)

--- scripts/base_models.R
+++ scripts/base_models.R
@@ -20,19 +20,16 @@ secondary <- c('mean' = 4.60 , 'sd' = 0.85)
 NPAIRS = 100
 pop <- InitPop(N = NPAIRS, 
                H2 = 0.33,
-               donor_vl = index, 
-               recipient_vl = secondary) %>%
-  cbind.data.frame(sex = sample(c('M', 'F'), nrow(.), replace = T)) %>%
-  cbind.data.frame(age = sample(18:50, nrow(.), replace = T))
-
+               transmitter_SpVL = index, 
+               recipient_SpVL = secondary) 
 
 
 ################################### Fit Heritability Model ###################################
-cat('Fitting heritablity model...')
+cat('Fitting heritablity model...\n')
 heritability_model <- brms::brm(
   # specify what to explain in terms of what
   #  using the formula syntax
-  formula = recipient_log10SPVL ~ 1 + transmitter_log10SPVL,
+  formula = recipient_log10SpVL ~ 1 + transmitter_log10SpVL,
   # which data to use
   data = pop
 )
@@ -45,10 +42,10 @@ plt_1a <- ggplot(pop, aes(x = transmitter, recipient)) +
     shape = 4, size = 3) +
   scale_x_log10(limits = c(1, 10**10),
                 expand = c(0,0),
-                name = expression(paste("Transmitter SPVL", ' (', Log[10], " copies ", ml**-1, ')')),
+                name = expression(paste("Transmitter SpVL", ' (', Log[10], " copies ", ml**-1, ')')),
                 breaks = trans_breaks("log10", function(x) 10**x),
                 labels = trans_format("log10", math_format(.x))) +
-  scale_y_log10(name = expression(paste("Recipient SPVL", ' (', Log[10], " copies ", ml**-1, ')')),
+  scale_y_log10(name = expression(paste("Recipient SpVL", ' (', Log[10], " copies ", ml**-1, ')')),
                 limits = c(1, 10**10),
                 expand = c(0,0),
                 breaks = trans_breaks("log10", function(x) 10**x),
@@ -61,8 +58,8 @@ plt_1a <- ggplot(pop, aes(x = transmitter, recipient)) +
 
 ################################### Fit Tolerance Model ###################################
 # Fits model as described by Regoes et al. 2014
-cat('Fitting tolerance model...')
-tolerance_model <- lm(CD4.decline ~ spVL + I(spVL**2), data = data)
+cat('Fitting tolerance model...\n')
+tolerance_model <- lm(CD4.decline ~ SpVL + I(SpVL**2), data = data)
 
 ##################################################
 # parameters from Regoes et al. PLoS Biology 2014 
@@ -70,12 +67,12 @@ tolerance_model <- lm(CD4.decline ~ spVL + I(spVL**2), data = data)
 # a_f = -0.010941 
 # n_m = - 0.000243 
 
-plt_1b <- ggplot(data, aes(x = spVL, y=CD4.decline))+
+plt_1b <- ggplot(data, aes(x = SpVL, y=CD4.decline))+
   geom_point(colour = '#ef654a', shape= 4, alpha = 0.3) +
   scale_y_continuous(name = expression(paste(Delta, ' CD4+ ', mu, l**-1, ' ', day**-1)),  #
                      expand = c(0,0),
                      limits = c(-2,2))+
-  scale_x_continuous(name = expression(paste("Recipient SPVL", ' (', Log[10], " copies ", ml**-1, ')')),
+  scale_x_continuous(name = expression(paste("Recipient SpVL", ' (', Log[10], " copies ", ml**-1, ')')),
                      expand = c(0,0))+
   geom_function(fun = function(x) ((-5.6e-3) + (-1.6e-4)*20) * x**2, colour = '#fdbb84', linewidth = 1.2, xlim = c(0,6.3)) +
   geom_text(aes(x = 6.55, y = -0.35), label = "20 yrs", colour = "#fdbb84", size = 6) +
@@ -88,7 +85,7 @@ plt_1b <- ggplot(data, aes(x = spVL, y=CD4.decline))+
 
 
 ################################### Run Transmission ###################################t
-cat('Simulating transmission model...')
+cat('Simulating transmission model...\n')
 baseline_tm <- populationmodel_acrossVL_Environment(sp_ViralLoad = 10**6, w = 1)  %>%
   setNames(nm = c('variant_distribution','probTransmissionPerSexAct','transmitter',  'w'))
  
@@ -137,11 +134,16 @@ plt_1d <- ggplot(joint_probs, aes(y = variants, x = nparticles))+
         legend.title = element_text(family = 'sans'))
 
 ################################### Export Plots ###################################
-cat('Preparing outputs...')
-ggsave(plot = plt_1a, filename = paste(figs_dir,sep = '/', "plt_1a.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
-ggsave(plot = plt_1b, filename =  paste(figs_dir,sep = '/',"plt_1b.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
-ggsave(plot = plt_1c, filename =  paste(figs_dir,sep = '/',"plt_1c.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
-ggsave(plot = plt_1d, filename =  paste(figs_dir,sep = '/',"plt_1d.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
+cat('Preparing outputs...\n')
+#ggsave(plot = plt_1a, filename = paste(figs_dir,sep = '/', "plt_1a.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1b, filename =  paste(figs_dir,sep = '/',"plt_1b.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1c, filename =  paste(figs_dir,sep = '/',"plt_1c.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1d, filename =  paste(figs_dir,sep = '/',"plt_1d.eps"), device=cairo_ps, width = 10, height = 10, units= 'in')
+ 
+#ggsave(plot = plt_1a, filename = paste(figs_dir,sep = '/', "plt_1a.jpeg"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1b, filename =  paste(figs_dir,sep = '/',"plt_1b.jpeg"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1c, filename =  paste(figs_dir,sep = '/',"plt_1c.jpeg"), device=cairo_ps, width = 10, height = 10, units= 'in')
+#ggsave(plot = plt_1d, filename =  paste(figs_dir,sep = '/',"plt_1d.jpeg"), device=cairo_ps, width = 10, height = 10, units= 'in')
 
-cat('base_models.R complete.')
+cat('base_models.R complete.\n')
 ## END ##
