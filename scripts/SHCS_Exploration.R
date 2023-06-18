ac2bcb539186048ceec2502393bca0a79249f0a5
J-Baxter <james.baxter@ed.ac.uk>
2023-06-18 01:52:18 +0100

    corrected factor levels of output; suspect new levels are associated with random effect specification

--- scripts/SHCS_Exploration.R
+++ scripts/SHCS_Exploration.R
@@ -126,7 +126,8 @@ sim_data <- mvtnorm::rmvnorm(n = 100, mean = as.vector(colMeans(shcs_data_int)),
                                riskgroup == 5 ~ "OTHER")) %>%
   mutate(across(where(is.character), .fns = ~as.factor(.x))) %>%
   mutate(sex = relevel(sex, 'M')) %>%
-  mutate(riskgroup = factor(riskgroup, levels = c('PWID', 'HET', 'MSM', 'OTHER', 'UNKNOWN')))
+  mutate(riskgroup = factor(riskgroup, levels = c('PWID', 'HET', 'MSM', 'OTHER', 'UNKNOWN'))) %>%
+  filter(min(shcs_data_long$log10_SpVL_couplemean)<log10_SpVL_couplemean && log10_SpVL_couplemean<max(shcs_data_long$log10_SpVL_couplemean))
 
-preds <- predict(adjusted_model, newdata = sim_data) #newlevels detected in data
-
+preds <- predict(adjusted_model, newdata = sim_data) #newlevels detected in data - current suspicion is random effects?
+ranef(adjusted_model)
