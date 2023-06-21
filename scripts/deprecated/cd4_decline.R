aeee35f007db10323fd764e01190d7d3d6fe2e2a
J-Baxter <james.baxter@ed.ac.uk>
2023-06-21 18:10:32 +0100

    updated terms from Regoes et al

--- scripts/deprecated/cd4_decline.R
+++ scripts/deprecated/cd4_decline.R
@@ -2,27 +2,25 @@
 # from Disentangling Human Tolerance and Resistance Against HIV
 # Regoes et al. PLoS Biology 2014
 # CD4 decline is 'change in CD4+ T cells per microlitre per day'
+# Incorporates terms for age and recipient sex
 
-
-cd4_data <- read_delim('./data/pbio.1001951.s006.tsv', delim = '\t' )
-
-# -0.1 + 0.051*log10(V) - 0.017*(log10(V))**2
-cd4_model <- lm(CD4.decline ~ spVL + I(spVL**2), data = cd4_data )
-#why does this fit differently to Poly(spVL, 2)?
-summary(cd4_model)
-confint(cd4_model, level = 0.95)
-
-
-CD4Decline <- function(log10SPVL,age){
+ToleranceModel <- function(log10SPVL, age, sex){
   
+  male <- ifelse(sex == 'M', 1, 0)
   ##################################################
   # parameters from Regoes et al. PLoS Biology 2014
   ##################################################
   
-  alpha_0 = -5.6e-3
-  c = -1.6e-4
+  #alpha_0 = -5.6e-3
+  #c = -1.6e-4
+  #deltaCD4 <- (alpha_0 + c*age)*(log10SPVL)**2
   
-  deltaCD4 <- (alpha_0 + c*age)*(log10SPVL)**2
+  alpha_0_F = -5.866e-3 # tolerance at birth for females
+  eta_0_M = 4.91e-4 # sex difference of tolerance at birth
+  c_F = - 1.67e-4 # tolerance change per life years for females
+  z_M = -1e-6 # sex difference between the change of tolerance per life year
+  
+  deltaCD4 <- (alpha_0_F + male*eta_0_M + age * (c_F + male*z_M)) * (log10SPVL)**2
   
   return(deltaCD4)
 } 
