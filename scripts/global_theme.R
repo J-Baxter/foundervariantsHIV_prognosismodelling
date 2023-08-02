4a93cbcf98fefbe4cc937e8abf4a60a5f072ee29
J-Baxter <james.baxter@ed.ac.uk>
2023-08-02 10:01:45 +0100

    global theme set for ms

--- scripts/global_theme.R
+++ scripts/global_theme.R
@@ -7,9 +7,9 @@ require(showtextdb)
 font_add("lmsans10", 'lmsans10-regular.otf')
 showtext_auto()
 
-my_theme <- theme_classic(base_family = "lmsans10")+
+my_theme <- theme_classic(base_family = "lmsans10", base_size = 10)+
   theme(
-    text = element_text(size=16),
+    #text = element_text(size=10),
     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
     legend.position = 'none', 
