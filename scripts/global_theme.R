b88defa41fb63ae2948f13f1ffa54c7c7f962959
James Baxter <james.baxter@ed.ac.uk>
2023-08-02 15:46:06 +0100

    explicit definition of text size

--- scripts/global_theme.R
+++ scripts/global_theme.R
@@ -7,11 +7,15 @@ require(showtextdb)
 font_add("lmsans10", 'lmsans10-regular.otf')
 showtext_auto()
 
-my_theme <- theme_classic(base_family = "lmsans10", base_size = 10)+
+my_theme <- theme_classic(base_family = "lmsans10")+
   theme(
     #text = element_text(size=10),
-    axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)),
-    axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
+    axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 8),
+    axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = 8),
+    axis.text = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 7),
+    strip.text  = element_text(size = 8),
+    legend.text = element_text(size = 7),
+    legend.title = element_text(size = 8),
     legend.position = 'none', 
     panel.spacing = unit(2, "lines"), 
     strip.background = element_blank()
