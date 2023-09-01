1409e7aedd681291545ed287d0a387ec74db0b19
J-Baxter <james.baxter@ed.ac.uk>
2023-09-01 12:20:45 +0100

    general update

--- scripts/interpret_outputs.R
+++ scripts/interpret_outputs.R
@@ -151,7 +151,68 @@ ggplot(test_function) +
   scale_x_continuous('Days Post Infection') + 
   scale_y_continuous('Proportion of Cohort with < 350 CD4 mm3') +
   coord_cartesian(xlim = c(0,365*10))+ #cut at 10 years
-  my_theme
+  my_theme +
+  theme(legend.position = 'right')
 
 # Log rank or CPH - 
+iskgroup_recipient)) == 1
+length(unique(data$transmitterallocation)) == 1
+z=data.frame( p_variants_1 = 1-rep(seq(0.25, 0.32, length.out = 27), 2), delta_CD4_recipient= c(seq(-0.31, -0.18, by = 0.005), seq(-0.3, -0.17, by = 0.005)),
+            riskgroup_recipient = 'N',
+            transmitterallocation = 'X')
+
+
+z=data.frame( delta_CD4_recipient= seq(-0.31, -0.18, by = 0.005), 
+              p_variants_1 = 1-c(0.25,0.255, 0.26, 0.265, 0.26, 0.27,0.275, 0.28, 0.275, 0.28, 0.30, 0.29, 
+      0.305, 0.31, 0.315, 0.31,0.30, 0.315,0.31, 0.305,0.31, 0.315,0.30, 0.32,0.32, 0.32,0.32),
+      riskgroup_recipient = 'N',
+      transmitterallocation = 'X') %>%
+  bind_rows(.,.,.) %>%
+  mutate(p_variants_1 = jitter(p_variants_1, factor = 3)) %>%
+  mutate(delta_CD4_recipient= jitter(delta_CD4_recipient, factor = 15))
+
+
+z_func <- GetCD4Survival(z, replicates = 100)
+
+
+z_plota <- ggplot(z) +
+  geom_point(aes(x = 1-p_variants_1 , y= delta_CD4_recipient ),colour = '#ef654a' ,shape = 4) +
+  scale_x_continuous(name = 'P(Multiple Variant Recipient)',
+                     expand = c(0.02,0.02),
+                     limits = c(0,0.4),
+                     breaks = seq(0, 0.4, by = 0.1))+
+  scale_y_continuous(name = expression(paste(Delta, ' CD4+ ', mu, l**-1, ' ', day**-1)),  #
+                     expand = c(0,0),
+                     limits = c(-0.5,0),
+                     axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 8),
+                     axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = 8),
+                     axis.text = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 7),
+                     strip.text  = element_text(size = 8),
+                     legend.text = element_text(size = 7),
+                     legend.title = element_text(size = 8),) +
+  my_theme 
+
+z_plotb <- ggplot(z_func) +
+  geom_line(aes(x = time, y= `0.5`, colour = multiplicity)) +
+  geom_ribbon(aes(x = time, ymin = `0.01`, ymax = `0.99`, fill = multiplicity), alpha = 0.5)+
+  scale_x_continuous('Days Post Infection') + 
+  scale_y_continuous('Proportion of Cohort with < 350 CD4 mm3') +
+  scale_color_manual(values = c("#fdbb84","#ef6548")) +
+  scale_fill_manual(values = c("#fdbb84","#ef6548")) +
+  coord_cartesian(xlim = c(0,365*10))+ #cut at 10 years
+  my_theme +
+  theme(legend.position = c(0.9,0.7),
+        axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 8),
+        axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = 8),
+        axis.text = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 7),
+        strip.text  = element_text(size = 8),
+        legend.text = element_text(size = 7),
+        legend.title = element_text(size = 8),)
+
+ggsave(plot = cowplot::plot_grid(z_plota, z_plotb , labels = 'AUTO', nrow = 1, align = 'hv', label_size = 10) , filename =  "zplot.jpeg", 
+       device = jpeg,  width = 180, height = 80,  units = 'mm')
+
+ggplot(z) + geom_point(aes(x=x, y=y))
+
+
 
